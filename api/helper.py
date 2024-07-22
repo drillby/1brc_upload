3d9df7a66dfccb9b167e0927894b85e5ea55c566
Pavel Podrazký <podrazkypavel002@gmail.com>
2024-07-22 16:02:54 +0200

    running script and sending res

--- api/helper.py
+++ api/helper.py
@@ -9,18 +9,25 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from os import PathLike
 from pathlib import Path
-from typing import Iterable, Optional, Union
+from typing import Callable, Iterable, Optional, Union
+
+from api import app
 
 from .messages import EmailMessages
-from .models.user import User
+from .models.user import User, db
 
 
 def allowed_file(filename: str, allwed_extensions: Iterable[str]) -> bool:
     return "." in filename and filename.rsplit(".", 1)[1].lower() in allwed_extensions
 
 
-def run_script(file_path, timeout, action):
+def allowed_email_domain(email: str, allowed_domains: Iterable[str]) -> bool:
+    return email.rsplit("@", 1)[1].lower() in allowed_domains
+
+
+def run_script(file_path: PathLike, timeout: int, action: Callable) -> None:
     try:
+        # run script
         start = time.time()
         res = subprocess.run(
             ["python", file_path],
@@ -29,37 +36,81 @@ def run_script(file_path, timeout, action):
             timeout=timeout,
             check=True,
         )
-        # run file_path python script as different subprocess
 
         end = time.time()
         runtime = end - start
-        print(res)
         if res.returncode != 0:
             raise Exception(res.stderr)
-        else:
-            # validate output
-            print(res.stdout)
-        print("Runtime: ", runtime)
+        with open("uploads/data/correct_output.txt", "r") as f:
+            correct_output = f.read().strip()
+        output = res.stdout.strip()
 
-        print("Sending email")
-        action(
-            body=EmailMessages.SUCCESS.value.format(runtime=runtime)
-            + "\n"
-            + EmailMessages.FOOTER.value
+        is_same = compare_strings(correct_output, output)
+
+        base_message = (
+            EmailMessages.SUCCESS.value.format(runtime=runtime)
+            if is_same
+            else EmailMessages.DIFFERENT_RESULTS.value
         )
+        email_body = f"{base_message}{EmailMessages.NEW_LINE.value}{EmailMessages.MATCHING_RESULTS.value if is_same else EmailMessages.DIFFERENT_RESULTS.value}{EmailMessages.NEW_LINE.value}{EmailMessages.NEW_LINE.value}{EmailMessages.FOOTER.value}"
+
+        # send email with results
+        action(body=email_body)
+
+        # update best time if needed
+        with app.app_context():
+            user_to_update: User = User.query.filter_by(
+                email=action.keywords["email_address"]
+            ).first()
+
+            if (
+                user_to_update
+                and is_same
+                and (
+                    user_to_update.best_time == 0 or runtime < user_to_update.best_time
+                )
+            ):
+                user_to_update.best_time = runtime
+                user_to_update.file_name = Path(file_path).name
+                db.session.commit()
+
     except Exception as e:
         if isinstance(e, subprocess.TimeoutExpired):
+            # send email with timeout message
             action(
                 body=EmailMessages.TIMEOUT.value.format(timeout=timeout)
-                + "\n"
+                + EmailMessages.NEW_LINE.value
+                + EmailMessages.NEW_LINE.value
                 + EmailMessages.FOOTER.value
             )
         else:
+            # send email with runtime error message
             action(
                 body=EmailMessages.RUNTIME_ERROR.value.format(error=e)
-                + "\n"
+                + EmailMessages.NEW_LINE.value
+                + EmailMessages.NEW_LINE.value
                 + EmailMessages.FOOTER.value
             )
+    finally:
+        # delete files that are not in db
+        with app.app_context():
+            all_files = set(
+                [user.file_name for user in User.query.filter(User.file_name != None)]
+            )
+
+        all_files_in_uploads = set(
+            [
+                file.name
+                for file in Path(app.config["UPLOAD_FOLDER"]).rglob("*")
+                if file.is_file()
+            ]
+        )
+
+        files_to_delete = all_files_in_uploads - all_files
+        for file in files_to_delete:
+            if file in ("correct_output.txt", "measurements.txt"):
+                continue
+            Path(f"{app.config['UPLOAD_FOLDER']}/{file}").unlink()
 
 
 class SMTPHandler:
@@ -142,9 +193,8 @@ class SMTPHandler:
             server.sendmail(self.email_address, email_address, message.as_string())
 
 
-def hash_password(password: str) -> str:
-    return hashlib.sha512(password.encode()).hexdigest()
+def compare_strings(s1: str, s2: str) -> bool:
+    if len(s1) != len(s2):
+        return False
 
-
-if __name__ == "__main__":
-    print(len(hash_password("password")))
+    return hashlib.md5(s1.encode()).hexdigest() == hashlib.md5(s2.encode()).hexdigest()
