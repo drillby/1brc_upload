bfbbf40c4ee8bf667e7fe1901321e080c263b103
Pavel Podrazký <podrazkypavel002@gmail.com>
2024-07-22 16:03:25 +0200

    updated model

--- api/models/user.py
+++ api/models/user.py
@@ -1,6 +1,7 @@
 import uuid
 
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
 
 db = SQLAlchemy()
 
@@ -13,14 +14,17 @@ class User(db.Model):
         default=uuid.uuid1,
     )
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(128), unique=False, nullable=False)
-    file_name = db.Column(db.String(120), unique=False, nullable=False)
-    time = db.Column(db.Integer, unique=False, default=0, nullable=False)
+    password_hash = db.Column(db.String(162), unique=False, nullable=False)
+    file_name = db.Column(db.String(120), unique=False, nullable=True)
+    best_time = db.Column(db.Float, unique=False, default=0, nullable=False)
+
+    def __init__(self, email, password, file_name=None) -> None:
+        self.email = email
+        self.password_hash = generate_password_hash(password)
+        self.file_name = file_name
 
     def __repr__(self):
         return f"<User {self.email}>"
 
-    def __init__(self, email, password, file_name) -> None:
-        self.email = email
-        self.password = password
-        self.file_name = file_name
+    def check_password(self, password: str) -> bool:
+        return check_password_hash(self.password_hash, password)
